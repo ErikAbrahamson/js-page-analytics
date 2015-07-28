72dccd0b84bbf1aa8c6cd39f7b4b50417fd90750
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-28 15:08:11 -0600

    Add timer image

--- js/main.js
+++ js/main.js
@@ -9,13 +9,22 @@ $(window).scroll(function() {
   var $windowTop = $(window).scrollTop();
   var $documentHeight = $(document).height();
   var $windowHeight = $(window).height();
-
-  $('#analytics-percent').text(($windowTop /($windowHeight - $documentHeight) * -100).toFixed(0) + '%');
+  var $scrollPercent = ($windowTop / ($documentHeight - $windowHeight) * 100).toFixed(0);
+  $('#analytics-percent').text($scrollPercent + '%');
 });
-// Time difference, button clicked
 
+// Time difference, button clicked
 $('#signup').on('click', function() {
   var $clickTime = $.now();
   var $duration = (($clickTime - $startTime) / 1000).toFixed(0);
   $('#analytics-time').text($duration + 's');
+});
+
+// Time spent on site
+$('#analytics-duration').on('click', function() {
+  var $clickTime = $.now();
+  var $duration = (($clickTime - $startTime) / 1000).toFixed(0);
+  for (var counter = 0; counter < 100000; counter += $duration) {
+    $('#analytics-duration').html($duration + 's');
+  }
 });
